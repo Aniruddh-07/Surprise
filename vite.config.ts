237678d3f018e1768ae4272f757fdec40506f219
Aniruddh-07 <upadhyayaniruddh482@gmail.com>
2026-02-12 22:31:46 +0530

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/Surprise/",
+  base: mode === "production" ? "/Surprise/" : "/", // 👈 FIX
 
   server: {
     host: "::",
